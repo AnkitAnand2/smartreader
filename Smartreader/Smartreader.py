b05d1eebd787c1bc57c2447cb534c8787f4d78da
AnkitAnand2 <54938737+AnkitAnand2@users.noreply.github.com>
2020-10-03 18:12:21 +0530

    Update Smartreader.py

--- Smartreader/Smartreader.py
+++ Smartreader/Smartreader.py
@@ -7,12 +7,14 @@ Created on Sat Oct  3 12:51:01 2020
 
 import pyttsx3 as ps
 import PyPDF2 as pdf
-book =open('marketing-mix-modeling-what-marketers-need-to-know.pdf','rb')
-pdfReader=pdf.PdfFileReader(book)
-speaker=ps.init()
-inputpage=int(input("Enter the page Number : "))
-page=pdfReader.getPage(inputpage-1)
-text=page.extractText()
-speaker.say(text)
-speaker.runAndWait()
-
+def readfile():
+    file_path=input('Enter the path of file: ')
+    file_name=input('Enter the pdf file name: ')
+    book =open(file_path+'\\'+file_name+'.pdf','rb')
+    pdfReader=pdf.PdfFileReader(book)
+    speaker=ps.init()
+    inputpage=int(input("Enter the page Number : "))
+    page=pdfReader.getPage(inputpage-1)
+    text=page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
